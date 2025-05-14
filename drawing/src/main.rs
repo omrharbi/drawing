020265dc424a761048499da1b46b21c2430660d9
omrharbi <rharbuomar@gmail.com>
2025-05-14 17:16:03 +0100

    done

--- drawing/src/main.rs
+++ drawing/src/main.rs
@@ -9,22 +9,22 @@ fn main() {
 
     gs::Line::random(image.width, image.height).draw(&mut image);
  
-    // gs::Point::random(image.width, image.height).draw(&mut image);
+    gs::Point::random(image.width, image.height).draw(&mut image);
 
-    // let mut rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
-    // rectangle.draw(&mut image);
+    let mut rectangle = gs::Rectangle::new(&gs::Point::new(150, 150), &gs::Point::new(50, 50));
+    rectangle.draw(&mut image);
 
-    // let mut triangle: geometrical_shapes::Triangle = gs::Triangle::new (
-    //         &gs::Point::new(500, 500),
-    //         &gs::Point::new(250, 700),
-    //         &gs::Point::new(700, 800),
-    // );
-    // // triangle.debug();
-    // triangle.draw(&mut image);
+    let mut triangle: geometrical_shapes::Triangle = gs::Triangle::new (
+            &gs::Point::new(500, 500),
+            &gs::Point::new(250, 700),
+            &gs::Point::new(700, 800),
+    );
+    // triangle.debug();
+    triangle.draw(&mut image);
 
-    // for _ in 1..50 {
-    //     gs::Circle::random(image.width, image.height).draw(&mut image);
-    // }
+    for _ in 1..50 {
+        gs::Circle::random(image.width, image.height).draw(&mut image);
+    }
 
       raster::save(&image, "image.png").unwrap();
 }
